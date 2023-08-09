b12a55274ea39bdfa6fc7d01c386744ccb2e1e7f
LeeHeonWoo1 <gjsdn245@naver.com>
2023-08-09 09:40:42 +0900

    delete example file

--- DoMySelf/dancy/use_class.py
+++ DoMySelf/dancy/use_class.py
@@ -1,10 +1,10 @@
 from get_angle_data import GetData
 
 data_path = ["./DoMySelf/dancy/video_data/SclassChallenge.mp4", "./DoMySelf/dancy/video_data/KnockChallenge.mp4"]
-down_url = "https://www.youtube.com/shorts/kX0UT7noimc"
+down_url = "https://www.youtube.com/watch?v=pAcShxBxNg0&t=83s"
 
-# get = GetData(save_path = "./validation_video.mp4", data_path = data_path, save_csv_path='./validation.csv', youtube_url=down_url)
-# get.download_youtube_video()
+get = GetData(save_path = "./validation_video.mp4", data_path = data_path, save_csv_path='./validation.csv', youtube_url=down_url)
+get.download_youtube_video()
 # get.make_angle_df()
 
 get = GetData(data_path = data_path, save_csv_path='./train.csv')
